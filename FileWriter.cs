db3514876bef1db4c7bcac903c216fe6bcf4b9e6
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2023-03-31 20:43:45 +0300

    Completed the first task

---
+++ FileWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteAndReadTextFile
+{
+    internal class FileWriter
+    {
+        internal void Write(string text, string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ LineReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WriteAndReadTextFile
+{
+    internal class LineReader
+    {
+        public string path;
+        FileWriter writer = new FileWriter();
+        internal void Read()
+        {
+            string line = Console.ReadLine();
+            if (line[0] == '%' & line[line.Length-1] == '%')
+            {
+                if (line == "%read%")
+                {
+                    try
+                    {
+                        foreach (string s in File.ReadAllLines(path))
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                } else
+                {
+                    if (line == "%close%")
+                    {
+                        Process.GetCurrentProcess().Kill();
+                    } else
+                    {
+                        if (line == "%help%")
+                        {
+                            Console.WriteLine("%help% - список команд с описанием\n" +
+                                "%read% - прочитать содержание файла\n" +
+                                "%close% - завершить работу программы");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неизвестная комманда");
+                        }
+                    }
+                }
+            } else
+            {
+                writer.Write(line, path);
+            }
+            
+        }
+    }
+}
