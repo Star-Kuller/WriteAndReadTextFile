11726c387624c24cbab0d00fab0a1ca2dc118838
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2023-04-18 05:16:06 +0300

    Revert "Completed the first task"
    This reverts commit db3514876bef1db4c7bcac903c216fe6bcf4b9e6.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WriteAndReadTextFile
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Введите путь к папке, где необходимо создать/открыть файл: ");
-            FileOpener opener = new FileOpener();
-            LineReader lineReader = new LineReader();
-            string path = Console.ReadLine();
-            path += "/test.txt";
-            opener.Open(path);
-            lineReader.path=path;
-            Console.WriteLine("Вводите текст или команду (%help% - список команд):");
-            while(true)
-            {
-                lineReader.Read();
-            }
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
